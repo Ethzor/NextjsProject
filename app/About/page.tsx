538a913479899ed55b9a6e3620f88a871ac5fd08
Ethzor <ethan.d.olivier@gmail.com>
2025-06-17 23:46:40 +0200

    First few changes

---
+++ app/About/page.tsx
@@ -0,0 +1,10 @@
+export default function About(): any {
+  return (
+    <section>
+      <div className="mx-auto m-5 bg-blue-500 text-white rounded-md p-4 w-150">
+        <h1 className="text-3xl">This is a test about heading</h1>
+        <p>this is a test about bio</p>
+      </div>
+    </section>
+  );
+}
